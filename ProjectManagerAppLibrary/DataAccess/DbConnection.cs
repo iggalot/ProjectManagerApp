45a1219d25872358cb827dcc229729af7137e3a9
Iggalot <iggalot.iggy@gmail.com>
2022-03-22 00:32:43 -0500

    Create basic models.  Create DB Collections.  Define DBConnection.
    Based on IATC ep. 8 & 9
    Remember to update the "SECRETS" json file to overwrite appsettings.json for "ConnectionStrings" and MongoDB connection string.

--- ProjectManagerAppLibrary/DataAccess/DbConnection.cs
+++ ProjectManagerAppLibrary/DataAccess/DbConnection.cs
@@ -1,5 +1,35 @@
 ﻿
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
 namespace ProjectManagerAppLibrary.DataAccess;
 public class DbConnection
 {
+   private readonly IConfiguration _config;
+   private readonly IMongoDatabase _db;
+   private string _connectionId = "MongoDB";
+   public string DbName { get; private set; }
+   public string CategoryCollectionName { get; private set; } = "categories";
+   public string StatusCollectionName { get; private set; } = "statuses";
+   public string UserCollectionName { get; private set; } = "users";
+   public string ProjectInfoCollectionName { get; private set; } = "projects";
+
+   public MongoClient Client { get; private set; }
+   public IMongoCollection<CategoryModel> CategoryCollection { get; private set; }
+   public IMongoCollection<StatusModel> StatusCollection { get; private set; }
+   public IMongoCollection<UserModel> UserCollection { get; private set; }
+   public IMongoCollection<ProjectInfoModel> ProjectInfoCollection { get; private set; }
+
+   public DbConnection(IConfiguration config)
+   {
+      _config = config;
+      Client = new MongoClient(_config.GetConnectionString(_connectionId));
+      DbName = _config["DatabaseName"];
+      _db = Client.GetDatabase(DbName);
+
+      CategoryCollection = _db.GetCollection<CategoryModel>(CategoryCollectionName);
+      StatusCollection = _db.GetCollection<StatusModel>(StatusCollectionName);
+      UserCollection = _db.GetCollection<UserModel>(UserCollectionName);
+      ProjectInfoCollection = _db.GetCollection<ProjectInfoModel>(ProjectInfoCollectionName);
+   }
 }
--- ProjectManagerAppLibrary/GlobalUsings.cs
+++ ProjectManagerAppLibrary/GlobalUsings.cs
@@ -1,1 +1,3 @@
 ﻿global using ProjectManagerAppLibrary.Models;
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization.Attributes;
