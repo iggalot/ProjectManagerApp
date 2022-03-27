eab4cbdfa2be24df0199a4c243013c70be889776
Iggalot <iggalot.iggy@gmail.com>
2022-03-26 22:57:56 -0500

    Create Project Info page
    Based on IATC ep. 16

---
+++ ProjectManagerAppUI/Models/CreateProjectInfoModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectManagerAppUI.Models;
+
+public class CreateProjectInfoModel
+{
+   [Required]
+   [MaxLength(75)]
+   public string ProjectInfo { get; set; }
+
+   [Required]
+   [MinLength(1)]
+   [Display(Name = "Category")]
+   public string CategoryId { get; set; }
+
+   [MaxLength(500)]
+   public string Description { get; set; }
+
+
+}
